ce63bd14a482130c6a0ea46eb016e912c48b79d3
sunss <sss213018@163.com>
2022-03-21 20:18:31 +0800

    📦 update

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+function myInstanceof(left, right) {
+  // 获取对象的原型
+  let proto = Object.getPrototypeOf(left)
+  // 获取构造函数的 prototype 对象
+  let prototype = right.prototype
+
+  // 判断构造函数的 prototype 对象是否在对象的原型链上
+  while (true) {
+    if (!proto) return false
+    if (proto === prototype) return true
+    // 如果没有找到，就继续从其原型上找，Object.getPrototypeOf方法用来获取指定对象的原型
+    proto = Object.getPrototypeOf(proto)
+  }
+}
+
 function Person() {}
+var boy = new Person()
+console.log(boy instanceof Person) // true，因为 Object.getPrototypeOf(o) === C.prototype
+console.log(boy instanceof Object) // true，因为 Object.prototype.isPrototypeOf(o) 返回 true
 
-const boy = new Person()
-
-console.log(boy)
-console.log(boy === Person.constructor)
+console.log(myInstanceof(boy, Person))
+console.log(myInstanceof(boy, Object))
