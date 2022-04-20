16a8c62886624dc97824d1d3226a13f74bacb495
sunss <sss@dataqin.com>
2022-04-20 10:56:07 +0800

    📦 update

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-function debounce(fn, time) {
-  let timer = null
-  return function () {
-    clearTimeout(timer)
-    timer = setTimeout(() => {
-      fn.apply(this, arguments)
-    }, time)
+function maxDepth(str) {
+  let len = str.length
+  let map = 0
+  let res = 0
+  let dep = 1
+
+  for (let i = 0; i < len; i++) {
+    if (['(', '{', '['].includes(str[i])) {
+      map++
+    } else if ([')', '}', ']'].includes(str[i])) {
+      map--
+      dep++
+    }
+    res = Math.max(res, dep)
   }
+  if (map !== 0) {
+    return 0
+  }
+  return res
 }
 
-function throttle(fn, time) {
-  let timer = null
-  return function () {
-    if (timer) return
-
-    timer = setTimeout(() => {
-      fn.apply(this.arguments)
-      timer = null
-    }, time)
-  }
-}
+console.log(maxDepth('([]{()})'))
