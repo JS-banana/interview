996f25ae120605b505842ee769827bfd9bb898ca
sunss <sss@dataqin.com>
2022-05-12 14:14:06 +0800

    📦 算法：二叉树的最小深度

--- algorithm/111.二叉树的最小深度.ts
+++ algorithm/111.二叉树的最小深度.ts
@@ -19,6 +19,11 @@
  * }
  */
 
+// 1. 叶子节点的定义是左孩子和右孩子都为 null 时叫做叶子节点
+// 2. 当 root 节点左右孩子都为空时，返回 1
+// 3. 当 root 节点左右孩子有一个为空时，返回不为空的孩子节点的深度
+// 4. 当 root 节点左右孩子都不为空时，返回左右孩子较小深度的节点值
+
 function minDepth(root: TreeNode | null): number {
   // root 为空时
   if (root == null) return 0
